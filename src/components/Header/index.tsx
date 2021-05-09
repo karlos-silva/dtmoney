18aac529b29cafa8fe2fa39e14230ffb1bbea347
Karlos Silva <karlossilva194@gmail.com>
2021-05-09 16:18:23 -0300

    Add lib for modal and configure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 import logoImg from '../../assets/logo.svg'
+
 import { Container, Content } from './styles'
 
-export const Header = () => {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void
+}
+export const Header = ({onOpenNewTransactionModal}: HeaderProps) => {
+  
 return(
   <Container>
     <Content>
       <img src={logoImg} alt="dt money"/>
-      <button>
+      <button type="button" onClick={onOpenNewTransactionModal} >
         Nova transação
       </button>
+
+      
     </Content>
   </Container>
 )
