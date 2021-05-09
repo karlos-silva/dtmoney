f050e0e312756c5d34569a5f5291e2cd8c8ab574
Karlos Silva <karlossilva194@gmail.com>
2021-05-09 15:05:38 -0300

    Create connection with API using axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react"
+import { api } from "../../services/api"
 import { Container } from "./styles"
 
 export const TransactionsTable = () => {
-
+  useEffect(() => {
+    api.get('transactions')
+    .then(response => console.log(response.data))
+  }, [])
   return(
     <Container>
       <table>
