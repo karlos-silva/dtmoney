68bef8363e8dceec570bb9c0efeca4ed920862e2
Karlos Silva <karlossilva194@gmail.com>
2021-05-09 21:04:43 -0300

    List the transaction with api and formt the values

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,11 @@
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 import { Container, TransactionTypeContainer, RadioBox } from './styles'
 
 import CloseImage from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { useState } from "react";
+import { api } from "../../services/api";
 
 interface NewTransactionModalProps {
   isOpen: boolean
@@ -13,8 +14,22 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
   const [type, setType] = useState('deposit')
+  const [title, setTitle] = useState('')
+  const [value, setValue] = useState(0)
+  const [category, setCategory] = useState('')
 
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault()
 
+    const data = {
+      title,
+      value,
+      category,
+      type
+    }
+
+    api.post('transactions', data)
+  }
 
   return (
     
@@ -32,16 +47,20 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         <img src={CloseImage} alt="Fechar Modal" />
       </button>
 
-      <Container>
-        <h2>Cadastrar Transação</h2>
+      <Container onSubmit={handleCreateNewTransaction}>
+        <h2> Cadastrar Transação</h2>
 
         <input 
           type="text" 
-          placeholder="Título" 
+          placeholder="Título"
+          value={title} 
+          onChange={e => setTitle(e.target.value)}
         />
         <input 
           type="number" 
           placeholder="Valor" 
+          value={value} 
+          onChange={e => setValue(Number(e.target.value))}
         />
 
         <TransactionTypeContainer>
@@ -66,8 +85,10 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         </TransactionTypeContainer>
 
         <input 
-          type="number" 
+          type="text" 
           placeholder="Categoria" 
+          value={category} 
+          onChange={e => setCategory(e.target.value)}
         />
         <button type="submit">
           Cadastrar
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { api } from "../../services/api"
 import { Container } from "./styles"
 
+interface Transactions {
+  id: number
+  title: string
+  amount: number
+  type: string
+  category: string
+  createdAt: string
+}
+
 export const TransactionsTable = () => {
+  const [transactions, setTransactions] = useState<Transactions[]>([])
+
   useEffect(() => {
     api.get('transactions')
-    .then(response => console.log(response.data))
+    .then(response => setTransactions(response.data.transactions))
   }, [])
   return(
     <Container>
@@ -19,18 +30,19 @@ export const TransactionsTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento Website</td>
-            <td className="deposit">R$122.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}</td>
           </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>17/02/2021</td>
-          </tr>
+          ))}
         </tbody>
       </table>
     </Container>
